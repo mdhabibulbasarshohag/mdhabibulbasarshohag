9b6a57c56e0781c60cf6e67b829c60e76899dff4
mdhabibulbasarshohag <mdhabibulbasarshohag1@gmail.com>
2024-09-26 22:41:07 +0600

    profile

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import Profile from './Pages/Profile/Profile'
 
 function App() {
   return (
-    <div className='h-screen relative flex lg:justify-center lg:flex-row flex-col marker:'>
+    <div className='lg:h-screen md relative flex p-3 lg:justify-center lg:flex-row flex-col marker:'>
       <Circles />
-      <div className='lg:w-[30%] w-full'>
+      <div className='lg:w-[30%] border-2 rounded-lg border-purple-500 lg:mr-3 mb-3 lg:mb-0 w-full'>
         <Profile></Profile>
       </div>
-      <div className='lg:w-[70%] w-full'>
+      <div className='lg:w-[70%] border-2 rounded-lg border-purple-500 w-full'>
         <p>left</p>
       </div>
     </div>
